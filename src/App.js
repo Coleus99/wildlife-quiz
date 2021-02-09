a19ce5e01f599dc9beb8c2b8d757c0918e1daa07
coleus99 <christopherjohncoles@gmail.com>
2021-02-09 23:23:42 +0000

    try hash router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/home'
 import Results from './components/results'
 
 function App() {
   return (
-    <BrowserRouter basename='/'>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <div className="container">
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/results' component={Results} />
         </Switch>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
