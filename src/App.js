9dce362580d5173e63636ab876d736f15437dc4a
coleus99 <christopherjohncoles@gmail.com>
2021-02-09 23:45:19 +0000

    changed back to BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react'
-import { HashRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/home'
 import Results from './components/results'
 
 function App() {
   return (
-    <HashRouter basename='/'>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="container">
-        <Home />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/results' component={Results} />
         </Switch>
       </div>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
