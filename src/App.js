71a94ca8efb1a3f74d466357ab56baf3c5e77baf
coleus99 <christopherjohncoles@gmail.com>
2021-02-09 23:39:45 +0000

    gh-pages testing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import Home from './components/home'
 import Results from './components/results'
 
 function App() {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename='/'>
       <div className="container">
+        <Home />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/results' component={Results} />
--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,4 @@
-import { useEffect, useState} from 'react'
+import React, { useEffect, useState} from 'react'
 import Question from './question'
 
 const Home = () => {
